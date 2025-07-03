cf2fde424e76f76458ad9b6ba81376d4eadcd98f
SubodhJondhale <122435965+SubodhJondhale@users.noreply.github.com>
2025-07-03 13:08:01 +0530

    Update session-config.tsx

--- src/components/session-config.tsx
+++ src/components/session-config.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import { ModalitiesSelector } from "@/components/modalities-selector";
 import { VoiceSelector } from "@/components/voice-selector";
-import { TemperatureSelector } from "./temperature-selector";
-import { MaxOutputTokensSelector } from "./max-output-tokens-selector";
 import { ConfigurationFormFieldProps } from "./configuration-form";
-import { ModelSelector } from "./model-selector";
 export function SessionConfig({ form }: ConfigurationFormFieldProps) {
   return (
     <div className="space-y-4 pt-2">
